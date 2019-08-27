64c42c475d2ba105265e3797c7c0437f2cbc45dc
kalugo <bbkb4a3@gmail.com>
2019-08-27 19:09:55 +0900

    comment  out carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
-if Rails.env.production?
-  require 'carrierwave/storage/abstract'
-  require 'carrierwave/storage/file'
-  require 'carrierwave/storage/fog'
+# if Rails.env.production?
+#   require 'carrierwave/storage/abstract'
+#   require 'carrierwave/storage/file'
+#   require 'carrierwave/storage/fog'
 
-  CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region: 'ap-northeast-1'
-    }
+#   CarrierWave.configure do |config|
+#     config.storage = :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#       provider: 'AWS',
+#       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+#       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+#       region: 'ap-northeast-1'
+#     }
 
-    config.fog_directory  = 'jun3'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/jun3'
-  end
-end
+#     config.fog_directory  = 'jun3'
+#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/jun3'
+#   end
+# end
